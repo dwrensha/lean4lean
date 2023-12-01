3547182530735655636d95365ea6934d14f11b23
David Renshaw <dwrenshaw@gmail.com>
2023-12-01 15:57:19 -0500

    more in digits.lean

--- digits.lean
+++ digits.lean
@@ -3,14 +3,25 @@
 /--
  Computes the decimal digits of a natural number, in little-endian order.
  -/
- def decimalDigits : Nat → List Nat
- | 0 => []
- | n + 1 => ((n + 1) % 10 :: decimalDigits ((n + 1) / 10))
- decreasing_by exact
-   Nat.div_lt_self (Nat.succ_pos n) (Nat.succ_lt_succ (Nat.succ_pos 8))
+def decimalDigits : Nat → List Nat
+| 0 => []
+| n + 1 => ((n + 1) % 10 :: decimalDigits ((n + 1) / 10))
+decreasing_by exact
+  Nat.div_lt_self (Nat.succ_pos n) (Nat.succ_lt_succ (Nat.succ_pos 8))
 
 --#reduce decimalDigits 1234
 
 def main (args: List String) : IO Unit := do
   let n := (args.get! 0).toNat!
   IO.println (decimalDigits n)
+
+-- #eval decimalDigits 1234567890
+
+def decimalDigitsWithFuel : Nat → Nat → List Nat
+|     0, _ => []
+|     _, 0 => []
+| m + 1, n => (n % 10 :: decimalDigitsWithFuel m (n / 10))
+
+def decimalDigits' (x : Nat) : List Nat := decimalDigitsWithFuel x x
+
+--#reduce decimalDigits' 1234
