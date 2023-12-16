5740a83295a4eb06fb0da2f5633a64f32051fe53
David Renshaw <dwrenshaw@gmail.com>
2023-12-16 09:37:34 -0500

    simpler minus3

--- decrement.lean
+++ decrement.lean
@@ -1,13 +1,25 @@
 import Std
 import Lean4Lean.Reduce
 
-def minus3 : Nat → Nat
-| 0 => 0
-| n + 1 => minus3 (n - 2)
+def minus3' : Nat → List Nat
+| 0 => []
+| n + 1 => (n + 1) :: minus3' (n - 2)
 decreasing_by exact Nat.sub_lt_succ n 2
 
 --set_option maxHeartbeats 0 in
---#l4lreduce minus3 20
+--#reduce minus3' 10
+
+def minus3 : Nat → List Nat :=
+fun n ↦
+ Acc.rec
+  (fun x _hx ih ↦ match x with
+                | 0 => []
+                | m + 1 => (m + 1) :: (ih (m - 2) (Nat.sub_lt_succ m 2)))
+  (WellFounded.apply Nat.lt_wfRel.wf n)
 
 -- This builds a very large term. Try increasing the numeral!
 --#reduce (WellFounded.apply Nat.lt_wfRel.wf 4)
+
+--set_option maxRecDepth 4000 in
+--set_option maxHeartbeats 0 in
+--#l4lreduce minus3 20
