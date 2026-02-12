405cb487b2ad9ec8dab08f1e7036f78e53eb5498
David Renshaw <dwrenshaw@gmail.com>
2026-02-11 22:28:40 -0500

    add Natfix.lean

---
+++ Lean4Lean/Natfix.lean
@@ -0,0 +1,35 @@
+namespace WellFounded
+
+variable {α : Sort u}
+variable {motive : α → Sort v}
+variable (h : α → Nat)
+variable (F : (x : α) → ((y : α) → InvImage (· < ·) h y x → motive y) → motive x)
+
+/-- Helper gadget that prevents reduction of `Nat.eager n` unless `n` evalutes to a ground term. -/
+def Nat.eager (n : Nat) : Nat :=
+  if Nat.beq n n = true then n else n
+
+theorem Nat.eager_eq (n : Nat) : Nat.eager n = n := ite_self n
+
+theorem Nat.le_of_lt_add_one {n m : Nat} : n < m + 1 → n ≤ m := Nat.le_of_succ_le_succ
+
+protected theorem Nat.lt_add_one (n : Nat) : LT.lt n (HAdd.hAdd n 1) := Nat.le_refl (Nat.succ n)
+
+/--
+A well-founded fixpoint operator specialized for `Nat`-valued measures. Given a measure `h`, it expects
+its higher order function argument `F` to invoke its argument only on values `y` that are smaller
+than `x` with regard to `h`.
+
+In contrast to to `WellFounded.fix`, this fixpoint operator reduces on closed terms. (More precisely:
+when `h x` evalutes to a ground value)
+
+-/
+def Nat.fix : (x : α) → motive x :=
+  let rec go : ∀ (fuel : Nat) (x : α), (h x < fuel) → motive x :=
+    fun fuel x hfuel ↦
+     match fuel with
+     | Nat.zero => (Nat.not_succ_le_zero _ hfuel).elim
+     | Nat.succ f' => F x (fun y hy => go f' y (Nat.lt_of_lt_of_le hy (Nat.le_of_lt_add_one hfuel)))
+  fun x => go (Nat.eager (h x + 1)) x (Nat.eager_eq _ ▸ Nat.lt_add_one _)
+
+end WellFounded
--- decrement.lean
+++ decrement.lean
@@ -1,5 +1,6 @@
 import Std
 import Lean4Lean.Reduce
+import Lean4Lean.Natfix
 
 def minus3' : Nat → List Nat
 | 0 => []
@@ -28,45 +29,6 @@ fun n ↦
 #check (Nat.succ_le_succ (Nat.zero_le 0))
 
 #check Nat.eq_or_lt_of_le
-
-namespace WellFounded
-
-variable {α : Sort u}
-variable {motive : α → Sort v}
-variable (h : α → Nat)
-variable (F : (x : α) → ((y : α) → InvImage (· < ·) h y x → motive y) → motive x)
-
-/-- Helper gadget that prevents reduction of `Nat.eager n` unless `n` evalutes to a ground term. -/
-def Nat.eager (n : Nat) : Nat :=
-  if Nat.beq n n = true then n else n
-
-theorem Nat.eager_eq (n : Nat) : Nat.eager n = n := ite_self n
-
-theorem Nat.le_of_lt_add_one {n m : Nat} : n < m + 1 → n ≤ m := Nat.le_of_succ_le_succ
-
-protected theorem Nat.lt_add_one (n : Nat) : LT.lt n (HAdd.hAdd n 1) := Nat.le_refl (Nat.succ n)
-
-/--
-A well-founded fixpoint operator specialized for `Nat`-valued measures. Given a measure `h`, it expects
-its higher order function argument `F` to invoke its argument only on values `y` that are smaller
-than `x` with regard to `h`.
-
-In contrast to to `WellFounded.fix`, this fixpoint operator reduces on closed terms. (More precisely:
-when `h x` evalutes to a ground value)
-
--/
-def Nat.fix : (x : α) → motive x :=
-  let rec go : ∀ (fuel : Nat) (x : α), (h x < fuel) → motive x :=
-    fun fuel x hfuel ↦
-     match fuel with
-     | Nat.zero => (Nat.not_succ_le_zero _ hfuel).elim
-     | Nat.succ f' => F x (fun y hy => go f' y (Nat.lt_of_lt_of_le hy (Nat.le_of_lt_add_one hfuel)))
-  fun x => go (Nat.eager (h x + 1)) x (Nat.eager_eq _ ▸ Nat.lt_add_one _)
-
-
-#print Nat.brecOn
-
-end WellFounded
 
 def minus3'' : Nat → List Nat :=
 WellFounded.Nat.fix (fun x ↦ x) fun a a_1 ↦
